90a08a530d373063ed543a78026b9b795b30836b
steve zhang <871506263@qq.com>
2023-02-11 18:35:48 +0800

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import {
+  createRouter,
+  createWebHashHistory,
+  type RouteLocationNormalized
+} from "vue-router";
+import routes from "./routes";
+import { useCachedViewStoreHook } from "@/store/modules/cachedView";
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+});
+
+export interface toRouteType extends RouteLocationNormalized {
+  meta: {
+    title?: string;
+    noCache?: boolean;
+  };
+}
+
+router.beforeEach((to: toRouteType, from, next) => {
+  // 路由缓存
+  useCachedViewStoreHook().addCachedView(to);
+  next();
+});
+
+export default router;
