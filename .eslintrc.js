bde613fcad36535a4975cf37112c2a581d6ded20
steve zhang <871506263@qq.com>
2023-02-12 12:00:25 +0800

    eslint提交

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
+require("@rushstack/eslint-patch/modern-module-resolution")
 
 module.exports = {
   root: true,
@@ -7,34 +7,35 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/recommended',
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    // '@vue/eslint-config-prettier'
+    "plugin:vue/recommended",
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier"
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
-    jsxPragma: 'React',
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+    jsxPragma: "React",
     ecmaFeatures: {
       jsx: true
     }
   },
-  parser: 'vue-eslint-parser',
+  parser: "vue-eslint-parser",
   rules: {
-    'vue/multi-word-component-names': [
-      'error',
+    "vue/multi-word-component-names": [
+      "error",
       {
-        ignores: ['index'] // 需要忽略的组件名
+        ignores: ["index"] // 需要忽略的组件名
       }
-    ]
+    ],
+    "prettier/prettier": "off" // 关闭eslint-config-prettier
   },
   overrides: [
     {
-      files: ['*.html'],
-      processor: 'vue/.vue'
+      files: ["*.html"],
+      processor: "vue/.vue"
     }
   ]
-};
+}
